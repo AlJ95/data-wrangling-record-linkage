a0f477bcffd4815ab77561040373449e861fccca
AlJ95 <jan.albrecht1995@googlemail.com>
2022-06-28 08:41:10 +0200

    comment out printers

--- recordLinkage.py
+++ recordLinkage.py
@@ -29,16 +29,17 @@ import loadDataset
 
 for name in ["clean", "little-dirty"]:
     for records in [1000, 10000]:
-        for comp_func in [
-            comparison.jaccard_comp,
-            comparison.dice_comp,
-            comparison.bag_dist_sim_comp,
-            comparison.jaro_winkler_comp,
-            comparison.edit_dist_sim_comp
-        ]:
+        # for comp_func in [
+        #     comparison.jaccard_comp,
+        #     comparison.dice_comp,
+        #     comparison.bag_dist_sim_comp,
+        #     comparison.jaro_winkler_comp,
+        #     comparison.edit_dist_sim_comp
+        # ]:
             print("\n\n\n\n\n")
             print("#############################################")
-            print(f"Name:{name}\nRecords:{records}\nComparion Function:{comp_func.__name__}")
+            # print(f"Name:{name}\nRecords:{records}\nComparion Function:{comp_func.__name__}")
+            print(f"Name:{name}\nRecords:{records}")
             print("#############################################")
             # name = "little-dirty"
             # name = "clean"
@@ -107,13 +108,13 @@ for name in ["clean", "little-dirty"]:
             # The list of tuples (comparison function, attribute number in record A,
             # attribute number in record B)
             #
-            one_specific_comp_funct_list = [
-                (comp_func, 1, 1),  # First name
-                (comp_func, 2, 2),  # Middle name
-                (comp_func, 3, 3),  # Last name
-                (comp_func, 8, 8),  # Suburb
-                (comp_func, 10, 10),  # State
-            ]
+            # one_specific_comp_funct_list = [
+            #     (comp_func, 1, 1),  # First name
+            #     (comp_func, 2, 2),  # Middle name
+            #     (comp_func, 3, 3),  # Last name
+            #     (comp_func, 8, 8),  # Suburb
+            #     (comp_func, 10, 10),  # State
+            # ]
 
             exact_comp_funct_list = [(comparison.exact_comp, 1, 1),  # First name
                                      (comparison.exact_comp, 2, 2),  # Middle name
@@ -124,7 +125,6 @@ for name in ["clean", "little-dirty"]:
 
             approx_comp_funct_list = [(comparison.jaccard_comp, 1, 1),  # First name
                                       (comparison.dice_comp, 2, 2),  # Middle name
-                                      (comparison.jaccard_comp, 6, 6), # birthday
                                       (comparison.jaro_winkler_comp, 3, 3),  # Last name
                                       (comparison.bag_dist_sim_comp, 7, 7),  # Address
                                       (comparison.edit_dist_sim_comp, 8, 8),  # Suburb
@@ -188,7 +188,7 @@ for name in ["clean", "little-dirty"]:
 
             # Print blocking statistics
 
-            blocking.printBlockStatistics(blockA_dict, blockB_dict)
+            # blocking.printBlockStatistics(blockA_dict, blockB_dict)
 
             # -----------------------------------------------------------------------------
             # Step 3: Compare the candidate pairs
@@ -197,7 +197,7 @@ for name in ["clean", "little-dirty"]:
 
             sim_vec_dict = comparison.compareBlocks(blockA_dict, blockB_dict,
                                                     recA_dict, recB_dict,
-                                                    one_specific_comp_funct_list)
+                                                    approx_comp_funct_list)
 
             comparison_time = time.time() - start_time
 
@@ -208,20 +208,23 @@ for name in ["clean", "little-dirty"]:
 
             # Exact matching based classification
             #
-            class_match_set, class_nonmatch_set = \
-                classification.exactClassify(sim_vec_dict)
+            # class_match_set, class_nonmatch_set = \
+            #     classification.exactClassify(sim_vec_dict)
 
             # *********** In lab 5, explore different similarity threshold values *********
 
             # Similarity threshold based classification
-            #
+
             # sim_threshold = 0.5
+            sim_threshold = 0.7
+            #
             # class_match_set, class_nonmatch_set = \
             #             classification.thresholdClassify(sim_vec_dict, sim_threshold)
 
             # Minimum similarity threshold based classification
             #
             # min_sim_threshold = 0.5
+            # min_sim_threshold = 0.7
             # class_match_set, class_nonmatch_set = \
             #             classification.minThresholdClassify(sim_vec_dict,
             #                                                 min_sim_threshold)
@@ -234,12 +237,12 @@ for name in ["clean", "little-dirty"]:
 
             # Lower weights for middle name and state
             #
-            # weight_vec = [2.0, 1.0, 2.0, 2.0, 2.0, 1.0]
-
-            # class_match_set, class_nonmatch_set = \
-            #             classification.weightedSimilarityClassify(sim_vec_dict,
-            #                                                       weight_vec,
-            #                                                       sim_threshold)
+            weight_vec = [2.0, 1.0, 2.0, 2.0, 2.0, 1.0]
+            #
+            class_match_set, class_nonmatch_set = \
+                        classification.weightedSimilarityClassify(sim_vec_dict,
+                                                                  weight_vec,
+                                                                  sim_threshold)
 
             # A supervised decision tree classifier
             #
@@ -269,11 +272,11 @@ for name in ["clean", "little-dirty"]:
             pc = evaluation.pairs_completeness(cand_rec_id_pair_list, true_match_set)
             pq = evaluation.pairs_quality(cand_rec_id_pair_list, true_match_set)
 
-            print('Blocking evaluation:')
-            print('  Reduction ratio:    %.3f' % rr)
-            print('  Pairs completeness: %.3f' % pc)
-            print('  Pairs quality:      %.3f' % pq)
-            print('')
+            # print('Blocking evaluation:')
+            # print('  Reduction ratio:    %.3f' % rr)
+            # print('  Pairs completeness: %.3f' % pc)
+            # print('  Pairs quality:      %.3f' % pq)
+            # print('')
 
             # Linkage evaluation
             #
@@ -287,20 +290,20 @@ for name in ["clean", "little-dirty"]:
             recall = evaluation.recall(linkage_result)
             fmeasure = evaluation.fmeasure(linkage_result)
 
-            print('Linkage evaluation:')
-            print('  Accuracy:    %.3f' % accuracy)
-            print('  Precision:   %.3f' % precision)
-            print('  Recall:      %.3f' % recall)
-            print('  F-measure:   %.3f' % fmeasure)
-            print('')
+            # print('Linkage evaluation:')
+            # print('  Accuracy:    %.3f' % accuracy)
+            # print('  Precision:   %.3f' % precision)
+            # print('  Recall:      %.3f' % recall)
+            # print('  F-measure:   %.3f' % fmeasure)
+            # print('')
 
             linkage_time = loading_time + blocking_time + comparison_time + \
                            classification_time
-            print('Total runtime required for linkage: %.3f sec' % linkage_time)
-            print('Loading Time:\t\t %.3f sec' % loading_time)
-            print('Blocking Time:\t\t %.3f sec' % blocking_time)
-            print('Comparison Time:\t %.3f sec' % comparison_time)
-            print('Linkage Time:\t\t %.3f sec' % linkage_time)
+            # print('Total runtime required for linkage: %.3f sec' % linkage_time)
+            # print('Loading Time:\t\t %.3f sec' % loading_time)
+            # print('Blocking Time:\t\t %.3f sec' % blocking_time)
+            # print('Comparison Time:\t %.3f sec' % comparison_time)
+            # print('Linkage Time:\t\t %.3f sec' % linkage_time)
             # -----------------------------------------------------------------------------
 
             # End of program.
